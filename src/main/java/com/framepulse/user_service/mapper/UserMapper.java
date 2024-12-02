0be57d8989c855d42b0518f15033e80256041ae5
akshaydongaregit <akshaydongare1797@gmail.com>
2024-12-03 02:17:37 +0530

    refactoring for common-libs

--- src/main/java/com/framepulse/user_service/mapper/UserMapper.java
+++ src/main/java/com/framepulse/user_service/mapper/UserMapper.java
@@ -1,8 +1,9 @@
 package com.framepulse.user_service.mapper;
 
+import com.framepulse.common.mapper.AbstractMapper;
 import com.framepulse.user_service.dto.UserDto;
 import com.framepulse.user_service.entity.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
-public interface UserMapper extends AbstractMapper<User, UserDto>{}
+public interface UserMapper extends AbstractMapper<User, UserDto> {}
--- src/main/java/com/framepulse/user_service/service/UserService.java
+++ src/main/java/com/framepulse/user_service/service/UserService.java
@@ -1,17 +1,15 @@
 package com.framepulse.user_service.service;
 
+import com.framepulse.common.service.AbstractService;
 import com.framepulse.user_service.entity.User;
 import com.framepulse.user_service.repository.UserRepository;
 import jakarta.transaction.Transactional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
 
 @Service
 @Transactional
-public class UserService extends AbstractService<User, String, UserRepository>{
+public class UserService extends AbstractService<User, String, UserRepository> {
 
     @Override
     public User save(User user) {
